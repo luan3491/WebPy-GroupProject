cb6245605f67f491cd51daff323b41d068011f34
shyo1734 <shyo1734@bht-berlin.de>
2026-07-08 18:48:35 +0200

    CustomerService can hide&reveal comments/reviews, hidden reviews and their comments can't be seen by Users, hidden comments can't be seen by Users, CustomerService can see hidden comments/reviews, new Buttons on template gamepage for hide and reveal functions

--- settings_folder/urls.py
+++ settings_folder/urls.py
@@ -23,31 +23,27 @@ from ComputerGames import views as game_views
 from Shoppingcart import views as cart_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('', game_views.home, name='home'),
     path("i18n/", include("django.conf.urls.i18n")), # internalization and localization
     path('home/', game_views.home, name='home'),
-
     path('useradmin/', include('Useradmin.urls')),
     path('useradmin/', include('django.contrib.auth.urls')),
-
     path('library/', game_views.game_list, name='game_list'),
     path('game/<int:pk>/', game_views.game_detail, name='game_detail'),
     path('game/<int:pk>/pdf/', game_views.game_pdf, name='game_pdf'),
-
     path("review/<int:review_id>/edit/", game_views.edit_review, name="edit_review"),
     path("review/<int:review_id>/delete/", game_views.delete_review, name="delete_review"),
     path("review/<int:review_id>/deletecs/", game_views.delete_review_cs, name="delete_review_cs"),
-
+    path("review/<int:review_id>/hidecs/", game_views.hide_review_cs, name="hide_review_cs"),
+    path("review/<int:review_id>/revealcs/", game_views.reveal_review_cs, name="reveal_review_cs"),
     path("comment/<int:comment_id>/edit/", game_views.edit_comment, name="edit_comment"),
     path("comment/<int:comment_id>/delete/", game_views.delete_comment, name="delete_comment"),
     path("comment/<int:comment_id>/deletecs/", game_views.delete_comment_cs, name="delete_comment_cs"),
-
+    path("comment/<int:comment_id>/hidecs/", game_views.hide_comment_cs, name="hide_comment_cs"),
+    path("comment/<int:comment_id>/revealcs/", game_views.reveal_comment_cs, name="reveal_comment_cs"),
     path('review/<int:review_id>/vote/<str:vote_type>/', game_views.review_vote, name='review_vote'),
     path('comment/<int:comment_id>/<str:up_or_down>/', game_views.comment_vote, name='comment_vote'),
-
     path('review/<int:review_id>/comment/', game_views.add_review_comment, name='add_review_comment'),
-
     path('cart/', cart_views.show_shopping_cart, name='cart'),
     path('cart/pay/', cart_views.pay, name='shopping_cart_pay'),
     path('cart/item/<int:item_id>/<str:action>/', cart_views.change_quantity, name='change_quantity'),
